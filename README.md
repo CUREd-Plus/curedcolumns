4dea029fdf3e484705964b316e3b0ba72a007717
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-04-29 16:21:29 +0100

    Intial prototype

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# metadata-generator
+# Metadata generator
+
 CUREd+ metadata generator
+
+# Installation
+
+# Usage
+
+# Development
+
+## Emulated S3 bucket
+
+https://s3ninja.net/
+
+The test data should be mounted to `/home/sirius/data` 
+
+```bash
+docker run --volume /home/sirius/data:/tmp/sirius/data --publish 9444:9000 scireum/s3-ninja:latest
+```
---
+++ src/metadata_generator/__main__.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+from typing import Generator, Optional
+
+import pyarrow.parquet
+import boto3
+
+DESCRIPTION = """
+"""
+
+USAGE = """
+"""
+
+logger = logging.getLogger(__name__)
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=DESCRIPTION, usage=USAGE)
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-l', '--loglevel', default='WARNING')
+    parser.add_argument('bucket')
+    parser.add_argument('--prefix', required=False, default='')
+    parser.add_argument('--no-sign-request', action='store_true')
+    return parser.parse_args()
+
+
+def get_parquet_column_names(bucket: str, key: str):
+    """
+
+    """
+    # Use pyarrow to access the ParquetDataset
+    dataset = arrow.parquet.ParquetDataset(f"s3://{bucket}/{key}")
+
+    # Get column names from schema
+    return dataset.schema.names
+
+
+def list_parquet_files(bucket: str, prefix: str = None) -> Generator[str, None, None]:
+    """
+    This function yields the S3 key (path) of parquet files in an S3 bucket as a generator.
+
+    Args:
+        bucket: The name of the S3 bucket to search (str).
+        prefix: Folder
+
+    Yields:
+        The S3 key (path) of each parquet file found (str).
+    """
+    s3_client = boto3.client("s3")
+    paginator = s3_client.get_paginator("list_objects_v2")
+
+    logger.info("%s/%s", bucket, prefix)
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
+        # Loop through S3 objects (files)
+        for obj in page.get("Contents", []):
+            s3_key: str = obj["Key"]
+            if s3_key.endswith(".parquet"):
+                yield s3_key
+
+
+def main():
+    args = get_args()
+    logging.basicConfig(
+        format="%(name)s:%(asctime)s:%(levelname)s:%(message)s",
+        level=logging.INFO if args.verbose else args.loglevel
+    )
+
+    for file in list_parquet_files(args.bucket, prefix=args.prefix):
+        print(file)
+
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    main()
