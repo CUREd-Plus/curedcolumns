b0c26b341618a68943f388db0f358bfd95c14ac7
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-06-26 12:06:27 +0100

    fix: Connect using S3 session

--- src/curedcolumns/get_parquet_column_names.py
+++ src/curedcolumns/get_parquet_column_names.py
@@ -2,21 +2,24 @@ import logging
 from pathlib import Path
 from typing import Union
 
+import pyarrow.fs
 import pyarrow.parquet
 
 logger = logging.getLogger(__name__)
 
 
-def get_s3_parquet_schema(bucket: str, key: Union[str, Path]) -> pyarrow.Schema:
+def get_s3_parquet_schema(session, bucket: str, key: Union[str, Path]) -> pyarrow.Schema:
     """
     List the column names for a table
     """
 
     uri = f"s3://{bucket}/{key}/*.parquet"
 
+    file_system = pyarrow.fs.S3FileSystem(session=session)
+
     logger.info(uri)
 
     # Use pyarrow to access the metadata
-    dataset = pyarrow.parquet.ParquetDataset(uri)
+    data_set = pyarrow.parquet.ParquetDataset(uri, filesystem=file_system)
 
-    return dataset.schema
+    return data_set.schema
