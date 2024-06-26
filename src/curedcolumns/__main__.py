30f847bd58d91c425ae91ac114022575bce43f39
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-06-26 10:54:30 +0100

    fix: default arg value

--- src/curedcolumns/__main__.py
+++ src/curedcolumns/__main__.py
@@ -9,8 +9,7 @@ import boto3
 from curedcolumns.iter_files import iter_files
 
 DESCRIPTION = """
-This will list all the field names for all the data sets
-in a bucket on AWS S3 object storage.
+List all the field names for all the data sets in a bucket on AWS S3 object storage.
 """
 
 logger = logging.getLogger(__name__)
@@ -31,7 +30,7 @@ def get_args() -> argparse.Namespace:
     parser.add_argument('-l', '--loglevel', default='WARNING')
     parser.add_argument('bucket', type=bucket_name, help='S3 bucket location URI')
     parser.add_argument('--prefix', required=False,
-                        default='Limits the response to keys that begin with the specified prefix.')
+                        help='Limits the response to keys that begin with the specified prefix.')
     parser.add_argument('--no-sign-request', action='store_true')
     parser.add_argument('--profile', default=AWS_PROFILE, help='AWS profile to use')
     return parser.parse_args()
