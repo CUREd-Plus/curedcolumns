5fbfc64cfb2831a857fa30b5b850a18bbf1c71e6
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-06-26 11:53:00 +0100

    fix: Parquet data set URI

--- src/curedcolumns/get_parquet_column_names.py
+++ src/curedcolumns/get_parquet_column_names.py
@@ -1,12 +1,20 @@
+import logging
+from pathlib import Path
+from typing import Union
+
 import pyarrow.parquet
 
+logger = logging.getLogger(__name__)
 
-def get_s3_parquet_schema(bucket: str, key: str) -> pyarrow.Schema:
+
+def get_s3_parquet_schema(bucket: str, key: Union[str, Path]) -> pyarrow.Schema:
     """
     List the column names for a table
     """
 
-    uri = f"s3://{bucket}/{key}"
+    uri = f"s3://{bucket}/{key}/*.parquet"
+
+    logger.info(uri)
 
     # Use pyarrow to access the metadata
     dataset = pyarrow.parquet.ParquetDataset(uri)
