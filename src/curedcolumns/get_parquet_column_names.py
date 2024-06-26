c19a9ee70e108bd1931c2981d9f6dd380dda2137
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-06-26 14:47:01 +0100

    fix: remove file glob

--- src/curedcolumns/get_parquet_column_names.py
+++ src/curedcolumns/get_parquet_column_names.py
@@ -29,7 +29,7 @@ def get_s3_parquet_schema(session, bucket: str, key: Union[str, Path]) -> pyarro
     )
 
     # Build data set location
-    uri = f"{bucket}/{key}/*.parquet"
+    uri = f"{bucket}/{key}"
     logger.info(uri)
 
     # Use pyarrow to access the metadata
